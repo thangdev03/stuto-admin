6f8c9e39be8697d0469be94299197f2965f8a926
thangdev03 <127105305+thangdev03@users.noreply.github.com>
2024-02-22 19:01:02 +0700

    first init project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#9069FF',
+        secondaryColor: '#E0E9F8',
+        backgroundColor: '#E0E9F8',
+        boxBackground: '#FFFFFF',
+        textColor: '#222222',
+        textInactive: '#777777',
+      },
+      boxShadow: {
+        blockShadow: '1px 3px 4px 0px rgba(0, 0, 0, 0.10)',
+      }
+    },
+  },
+  plugins: [],
+}
+
