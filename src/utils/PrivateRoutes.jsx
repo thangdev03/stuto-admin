79f61b1386b1ad877bfba1bdde5fff76f38e7686
thangdev03 <nhatthangdev@gmail.com>
2024-04-19 12:04:29 +0700

    fix private route

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuthContext } from "../hooks/useAuthContext";
 
 const PrivateRoutes = () => {
     const user = JSON.parse(localStorage.getItem("user"));
-    const isAdmin = user.role == "admin";
-    return user && isAdmin ? <Outlet /> : <Navigate to="/login" />
+    const isAuth = user && user.role == "admin" ? true : false;
+    return  isAuth ? <Outlet /> : <Navigate to="/login" />
 }
 
 export default PrivateRoutes
